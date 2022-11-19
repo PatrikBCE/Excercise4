28b46814fe303f69ccc550c456a34fca6198b394
TUP <turcsanyipatrik@gmail.com>
2022-11-19 17:31:03 +0100

    feladat6

--- excercise4/Form1.cs
+++ excercise4/Form1.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Excel = Microsoft.Office.Interop.Excel;
 using System.Reflection;
+using System.Data.Entity.Migrations.Model;
 
 namespace excercise4
 {
@@ -17,9 +18,43 @@ namespace excercise4
         RealEstateEntities re = new RealEstateEntities();
         //List<Flat> Flats = new List<Flat>();
         List<Flat> flats;
+
+        Excel.Application xlApp; // A Microsoft Excel alkalmazás
+        Excel.Workbook xlWB; // A létrehozott munkafüzet
+        Excel.Worksheet xlSheet; // Munkalap a munkafüzeten belül
         void LoadData()
         {
             flats = re.Flat.ToList();
+        }
+
+        void CreateExcel()
+        {
+            try
+            {
+                xlApp = new Excel.Application();
+                xlWB = xlApp.Workbooks.Add();
+                xlSheet = xlWB.ActiveSheet;
+
+                CreateTable();
+
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Source + '\n' + ex.Message);
+                xlWB.Close(false);
+                xlApp.Quit();
+                xlApp = null;
+                xlWB = null;
+            }
+
+        }
+
+        private void CreateTable()
+        {
+            throw new NotImplementedException();
         }
 
         public Form1()
