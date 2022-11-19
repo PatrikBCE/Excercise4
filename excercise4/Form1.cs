951d13346147520ac8a576f22e29fe56a23596b0
TUP <turcsanyipatrik@gmail.com>
2022-11-19 17:45:18 +0100

    feladat7

--- excercise4/Form1.cs
+++ excercise4/Form1.cs
@@ -91,6 +91,23 @@ namespace excercise4
 
         }
 
+        private string GetCell(int x, int y)
+        {
+            string ExcelCoordinate = "";
+            int dividend = y;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            ExcelCoordinate += x.ToString();
+
+            return ExcelCoordinate;
+        }
+
         public Form1()
         {
             InitializeComponent();
