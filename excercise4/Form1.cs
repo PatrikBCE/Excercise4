6b5f79d53f6ed03d2223fe23be5623a9f791b2ac
TUP <turcsanyipatrik@gmail.com>
2022-11-19 17:20:22 +0100

    5,2

--- excercise4/Form1.cs
+++ excercise4/Form1.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Excel = Microsoft.Office.Interop.Excel;
-using System.Reflection
+using System.Reflection;
 
 namespace excercise4
 {
